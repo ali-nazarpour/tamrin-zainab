7558c29ba017e224bbf2a0c6123a88e672740e3c
Ali Nazarpoor <90612920+alinazarpoor@users.noreply.github.com>
2025-04-18 13:01:47 +0330

    updated

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar } from "../components/navbar";
+import { Navbar } from '../components/navbar';
 import { Outlet } from "react-router-dom";
 
 export const MainLayout: React.FC = () => {
